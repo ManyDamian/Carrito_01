c2401609b1559e7aa9bf67e9fef0c46f5056e64d
jose manuel <l21270630@tuxtla.tecnm.mx>
2025-02-25 18:53:27 -0600

    YA LE CAMBIE >:D

---
+++ app/src/main/java/com/example/carrito_01/MenuActivity.kt
@@ -0,0 +1,24 @@
+package com.example.carrito_01
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_1)
+
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        val btnSettings = findViewById<Button>(R.id.btnSettings)
+
+        btnPlay.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        btnSettings.setOnClickListener {
+            //Aqui pondremos el rediccionamiento a settings
+            //startActivity(Intent(this, SettingsActivity::class.java))
+        }
+    }
+}
