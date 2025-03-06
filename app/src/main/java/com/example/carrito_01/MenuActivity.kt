ad8de140ffa533961f7254cb33b3cf975f2b876f
glesantonio308 <Glesantonio308@gmail.com>
2025-03-06 15:12:53 -0600

    Nueva interfaz y cosas

--- app/src/main/java/com/example/carrito_01/MenuActivity.kt
+++ app/src/main/java/com/example/carrito_01/MenuActivity.kt
@@ -1,6 +1,9 @@
 package com.example.carrito_01
+
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,12 +15,17 @@ class MenuActivity : AppCompatActivity() {
         val btnPlay = findViewById<Button>(R.id.btnPlay)
         val btnSettings = findViewById<Button>(R.id.btnSettings)
 
+        // Cargar la animación desde el archivo XML
+        val scaleAnimation = AnimationUtils.loadAnimation(this, R.anim.scale_button)
+
+        // Aplicar la animación cuando se presionan los botones
         btnPlay.setOnClickListener {
+            it.startAnimation(scaleAnimation)  // Iniciar animación
             startActivity(Intent(this, MainActivity::class.java))
         }
 
         btnSettings.setOnClickListener {
-            //Aqui pondremos el rediccionamiento a settings
+            it.startAnimation(scaleAnimation)  // Iniciar animación
             startActivity(Intent(this, SettingsActivity::class.java))
         }
     }
