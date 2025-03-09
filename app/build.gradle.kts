0e781e6a45f3bfbea7fd0e2855f393905a965ffb
Cesar Farrera <l21270591@tuxtla.tecnm.mx>
2025-03-09 02:59:15 -0600

    === Ultimos ajustes a la interfaz, la proxima versión empezará a implementar funcionalidades que faltan ===

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.camera.camera2)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.preference.ktx)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
